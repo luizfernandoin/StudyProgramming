eea6da976651e4d85f63f1d911a75c3ed2c50ec7
Luiz Fernando <luizfernandoinfo100@gmail.com>
2024-03-21 16:07:34 -0300

    feat: adicionando saída de dados

--- JAVA/src/InputAndOutput/inputAndOutput.java
+++ JAVA/src/InputAndOutput/inputAndOutput.java
@@ -23,5 +23,24 @@ public class inputAndOutput {
 
         String saida = new String("System.out.printf()");
         System.out.printf("\n%s", saida); //Saída formatada, semelhante ao printf do C.
+
+
+        /*
+        -> java.util.Scanner ajuda a trabalhar com os tipos de entrada de dados.
+
+        Ele funciona com tokens, basicamente ele divide o input em pequenas partes/tipos
+        (inteiros, strings, arquivos, fluxos de dados). Além disso, ele quebra a linha em
+        pequenos pedaços de acordo com o delimitador: “ ” ou fim de linha padrão.
+        */
+
+        Scanner scanner = new Scanner(System.in);
+
+        float numF = scanner.nextFloat();
+        int num1 = scanner.nextInt();
+        byte byte1 = scanner.nextByte();
+        long lg1 = scanner.nextLong();
+        boolean b1 = scanner.nextBoolean();
+        double num2 = scanner.nextDouble();
+        String nome = scanner.nextLine();
     }
 }
